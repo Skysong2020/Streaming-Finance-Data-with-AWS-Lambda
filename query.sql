f55ab93c4332ea3a6dff14aa6709e4e12c9f35c8
Skysong2020 <73456174+Skysong2020@users.noreply.github.com>
2021-12-17 18:42:48 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,23 @@
-SELECT SPLIT_PART(ID,',',1) as name, max_high, SPLIT_PART(ID,',',3) as ts, SPLIT_PART(ID,',',2) as hour FROM
-(SELECT DISTINCT(ID), max_high FROM
-(SELECT CONCAT(name,',',hour,',',ts) AS ID, max_high FROM (SELECT A.name, A.hour, A.ts, B.max_high FROM (SELECT name, high, ts, SUBSTRING(ts, 12, 2) AS hour  FROM sta9760f2021_fh1_1_2021_12_17_04_19_32_4bb14156_e459_412b_a75a_5c4a61ae0813) A
-INNER JOIN (SELECT name, SUBSTRING(ts, 12, 2) AS hour, MAX(high) AS max_high FROM sta9760f2021_fh1_1_2021_12_17_04_19_32_4bb14156_e459_412b_a75a_5c4a61ae0813 GROUP BY name, SUBSTRING(ts, 12, 2)) B
-ON A.name = B.name AND A.hour = B.hour AND A.high = B.max_high)))
-ORDER BY name, hour;
+WITH Group1 AS 
+    (SELECT name, Round(high,2) As high, ts,SUBSTRING(ts,12,2) AS Hour, ts AS Datetime
+    FROM "sta9760f2021stockdata".
+"04"
+),
+    
+    Group2 AS 
+    (SELECT Group1.name AS Company,
+         MAX(Group1.high) AS High,
+         Group1.Hour AS Hour
+    FROM Group1
+    GROUP BY  Group1.name, Group1.Hour)
+   
+    
+SELECT DISTINCT Group2.Company, Group2.High, Group2.Hour, Group1.DateTime
+
+FROM Group1, Group2
+
+WHERE Group1.name = Group2.Company
+        AND Round(Group1.high,2) = Round(Group2.High,2)
+        AND Group1.Hour = Group2.Hour
+
+ORDER BY  Group2.Company, Group2.Hour
